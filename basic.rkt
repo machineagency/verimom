def2c111e55ee89cf547d3ed78f7495fd2079470
Jasper O'Leary <jhaazpr@gmail.com>
2019-11-10 16:42:23 -0800

    change basic to basically the same as IMP just to get it to run

--- basic.rkt
+++ basic.rkt
@@ -3,6 +3,7 @@
 (require "verimom.rkt")
 (provide (all-defined-out))
 
-; Define a path in verimom
-(path-closed (list (pt 0 0) (pt 20 0) (pt 20 20) (pt 0 20)))
+; Define a path in verimom - TODO: reconsider and/or remove args
+(verimom _ () : ()
+    (path-closed 'original (pt 0 0) (pt 20 0) (pt 20 20) (pt 0 20)))
 
