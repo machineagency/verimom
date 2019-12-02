056f5d96eca755cb36e63217a2ba9a49caec2c1b
Jasper O'Leary <jhaazpr@gmail.com>
2019-12-01 17:53:30 -0800

    arm crash without arm constraint

--- twohead/test_quantifiers.py
+++ twohead/test_quantifiers.py
@@ -1,4 +1,5 @@
 from z3 import *
+from math import *
 
 # Test piecewise intersection
 # f - unit slope triangle wave with peak at 5
@@ -63,3 +64,43 @@ if s.check() == sat:
     print(f'Collision t-val: {s.model()[t]}')
 else:
     print('No collision.')
+
+# Try r1 running into r2, with arm constraint
+s = Solver()
+r1x = Function('r1x', RealSort(), RealSort())
+r1y = Function('r1y', RealSort(), RealSort())
+r2x = Function('r2x', RealSort(), RealSort())
+r2y = Function('r2y', RealSort(), RealSort())
+
+s.add(And(x >= 0, x <= 10))
+s.add(And(y >= 0, y <= 10))
+s.add(And(t >= 0, t <= 10))
+
+s.add(Implies(t == 0, r1x(t) == 0))
+s.add(Implies(t == 0, r1y(t) == 0))
+s.add(Implies(t == 0, r2x(t) == 10))
+s.add(Implies(t == 0, r2y(t) == 0))
+
+# R2 positions
+s.add(ForAll([t], Implies(And(t > 0, t < 5 * sqrt(2)), r2x(t) == t)))
+s.add(ForAll([t], Implies(And(t > 0, t < 5 * sqrt(2)), r2y(t) == -t + 10)))
+s.add(ForAll([t], Implies(And(t >= 5 * sqrt(2)), r2x(t) == 5)))
+s.add(ForAll([t], Implies(And(t >= 5 * sqrt(2)), r2y(t) == 5)))
+
+# R1 positions
+s.add(ForAll([t], Implies(And(t > 0, t < 5 * sqrt(2)), r1x(t) == (3/2) * t)))
+s.add(ForAll([t], Implies(And(t > 0, t < 5 * sqrt(2)), r1y(t) == 0)))
+s.add(ForAll([t], Implies(And(t >= 5 * sqrt(2)), r1x(t) == 15/2)))
+s.add(ForAll([t], Implies(And(t >= 5 * sqrt(2)), r1y(t) == 2 * t - 5)))
+
+collision = And(r1x(t) == r2x(t), r1y(t) == r2y(t))
+s.add(collision)
+
+print('Running r1 crash without arm constraint, should pass.')
+if s.check() == sat:
+    print(s.model())
+    print(f'Collision t-val: {s.model()[t]}')
+else:
+    print('No collision.')
+
+
