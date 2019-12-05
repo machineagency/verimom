00f3f6fbe6c187200187c436ea2e5dd08aaa904a
Jasper O'Leary <jhaazpr@gmail.com>
2019-12-04 17:12:41 -0800

    more test cases

--- twohead/example_progs.py
+++ twohead/example_progs.py
@@ -56,3 +56,21 @@ moveTo(300, 20, 0, 2);
 moveTo(300, 100, 0, 2);
 """
 
+prog_segmented = """moveTo(20, 20, 0, 1);
+moveTo(40, 40, 0, 1);
+travel(0, 0, 0, 1);
+travel(40, 40, 0, 1);
+moveTo(60, 60, 0, 1);
+"""
+
+prog_segmented_wrong = """moveTo(20, 20, 0, 1);
+moveTo(40, 40, 0, 1);
+travel(0, 0, 0, 1);
+travel(100, 100, 0, 1);
+moveTo(60, 60, 0, 1);
+"""
+
+prog_merged = """moveTo(60, 60, 0, 1);
+travel(0, 0, 0, 1);
+"""
+
