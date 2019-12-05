5faa0e353c0e4c8a1ce77ce2a2f2400ff94ac7b4
Jasper O'Leary <jhaazpr@gmail.com>
2019-12-05 11:46:46 -0800

    synth testing harness

--- twohead/synthesizer.py
+++ twohead/synthesizer.py
@@ -6,10 +6,10 @@ from analyzer import Analyzer, LangUtil
 
 class Rewriter():
     def __init__(self, prog_t):
-        self.prog_t_text = prog_t[:]
-        self.prog_r_text = prog_r[:]
-        prog_t_stats = LangUtil.prog_text_to_statements(prog_t)
-        prog_r_stats = LangUtil.prog_text_to_statements(prog_r)
+        self.prog_t_text = prog_t
+        self.prog_r_text = prog_t[:]
+        prog_t_stats = LangUtil.prog_text_to_statements(self.prog_t_text)
+        prog_r_stats = LangUtil.prog_text_to_statements(self.prog_r_text)
         self.prog_t = LangUtil.statements_to_dicts(prog_t_stats)
         self.prog_r = LangUtil.statements_to_dicts(prog_r_stats)
 
@@ -27,4 +27,12 @@ class Rewriter():
 
     def random_walk(self, steps):
         pass
+
+    def get_prog_r_text(self):
+        return LangUtil.dicts_to_text(self.prog_r)
+
+if __name__ == '__main__':
+    rw = Rewriter(prog_safe_r1_set)
+    rw.random_walk(3)
+    print(rw.get_prog_r_text())
 
