98206f9d06ba5745cbd60bf56c73c789aa031823
Jasper O'Leary <jhaazpr@gmail.com>
2019-12-05 17:40:16 -0800

    operate on rp (R-prime), which is the current rewrite we should only use prog_r when we have accepted the rewrite (which we will do soon). upon accepting a rewrite, set r <- rp. if we do not accept a rewrite, set rp <- r

--- twohead/synthesizer.py
+++ twohead/synthesizer.py
@@ -8,16 +8,19 @@ class Rewriter():
     def __init__(self, prog_t):
         self.prog_t_text = prog_t
         self.prog_r_text = prog_t[:]
+        self.prog_rp_text = prog_t[:]
         prog_t_stats = LangUtil.prog_text_to_statements(self.prog_t_text)
         prog_r_stats = LangUtil.prog_text_to_statements(self.prog_r_text)
+        prog_rp_stats = LangUtil.prog_text_to_statements(self.prog_rp_text)
         self.prog_t = LangUtil.statements_to_dicts(prog_t_stats)
         self.prog_r = LangUtil.statements_to_dicts(prog_r_stats)
-        self.RANDOM_SEED = 9382107
+        self.prog_rp = LangUtil.statements_to_dicts(prog_rp_stats)
+        self.RANDOM_SEED = 4373172
         seed(self.RANDOM_SEED)
         self.analyzer = Analyzer((300, 300), (0, 0), (300, 0))
 
     def change_instr(self, line_num):
-        d = self.prog_r[line_num]
+        d = self.prog_rp[line_num]
         if d['instr'] == 'moveTo':
             d['instr'] = 'travel'
         elif d['instr'] == 'travel':
@@ -25,7 +28,7 @@ class Rewriter():
         d = LangUtil.update_dict_statement(d)
 
     def change_operand(self, line_num):
-        d = self.prog_r[line_num]
+        d = self.prog_rp[line_num]
         if d['instr'] == 'moveTo' or d['instr'] == 'travel':
             op_num = randint(0, 2)
             if op_num == 0:
@@ -46,18 +49,18 @@ class Rewriter():
 
     def swap_instr(self, line_num):
         other_line_num = self.get_random_line_num()
-        d0 = self.prog_r[line_num]
-        d1 = self.prog_r[other_line_num]
-        self.prog_r[line_num], self.prog_r[other_line_num] = d1, d0
+        d0 = self.prog_rp[line_num]
+        d1 = self.prog_rp[other_line_num]
+        self.prog_rp[line_num], self.prog_rp[other_line_num] = d1, d0
 
     def change_line(self, line_num):
-        d = self.prog_r[line_num]
+        d = self.prog_rp[line_num]
         instrs = ['moveTo', 'travel', 'sleep']
         d['instr'] = instrs[randint(0, 2)]
         if d['instr'] == 'moveTo' or d['instr'] == 'travel':
             d['x'] = randint(0, self.analyzer.X_LIM)
             d['y'] = randint(0, self.analyzer.Y_LIM)
-            d['r'] = randint(1, 3)
+            d['r'] = randint(1, 2)
         if d['instr'] == 'sleep':
             # TODO: either use canned immediates or constraint s operand
             # to total time taken
@@ -66,20 +69,23 @@ class Rewriter():
         d = LangUtil.update_dict_statement(d)
 
     def add_or_delete_line(self, line_num):
-        add_or_delete = randint(0, 3)
-        if add_or_delete == 0:
-            del self.prog_r[line_num]
+        add_or_delete = randint(0, 1)
+        if add_or_delete == 0 and len(self.prog_rp) > 1:
+            del self.prog_rp[line_num]
         else:
-            self.prog_r.insert(line_num, { 'instr' : 'noop' })
+            self.prog_rp.insert(line_num, { 'instr' : 'noop' })
             self.change_line(line_num)
+
+    def decide_accept_rewrite(self, prog_rp):
+        pass
 
     def random_walk(self, steps):
         NUM_PERMUTATIONS = 5
         for _ in range(0, steps):
             permutation_choice = randint(0, NUM_PERMUTATIONS)
             line_num = self.get_random_line_num()
-            print(f'Line num: {line_num}')
-            print(f'P: {permutation_choice}')
+            # print(f'Line num: {line_num}')
+            # print(f'P: {permutation_choice}')
             if permutation_choice == 0:
                 self.change_instr(line_num)
             if permutation_choice == 1:
@@ -91,16 +97,18 @@ class Rewriter():
             if permutation_choice == 4:
                 self.add_or_delete_line(line_num)
             # print(self.curr_rewrite)
+        self.prog_r = self.prog_rp.copy()
 
     def get_random_line_num(self):
-        return randint(0, len(self.prog_r) - 1)
+        return randint(0, len(self.prog_rp) - 1)
 
     @property
     def curr_rewrite(self):
-        return LangUtil.dicts_to_text(self.prog_r)
+        return LangUtil.dicts_to_text(self.prog_rp)
 
 if __name__ == '__main__':
     rw = Rewriter(prog_safe_r1_set)
     rw.random_walk(100)
     print(rw.curr_rewrite)
+    print(rw.analyzer.find_running_time(rw.curr_rewrite))
 
